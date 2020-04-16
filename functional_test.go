b4a351c2975ebea029569d679c84dfd2d4257fbc
KJ Tsanaktsidis <ktsanaktsidis@zendesk.com>
2020-04-16 17:24:41 +1000

    Use a docker-compose file to run the tests
    Also create the topics & other deps inside the test itself.

--- functional_test.go
+++ functional_test.go
@@ -1,10 +1,12 @@
 package sarama
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
 	"net"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 	"testing"
@@ -26,6 +28,7 @@ var (
 	proxyClient *toxiproxy.Client
 	Proxies     map[string]*toxiproxy.Proxy
 )
+
 
 func init() {
 	if os.Getenv("DEBUG") == "true" {
@@ -58,6 +61,10 @@ func init() {
 	}
 
 	kafkaRequired = os.Getenv("CI") != ""
+
+	if kafkaAvailable {
+		createTestTopics()
+	}
 }
 
 func checkKafkaAvailability(t testing.TB) {
@@ -143,4 +150,113 @@ func parseKafkaVersion(version string) kafkaVersion {
 	}
 
 	return result
+}
+
+func createTestTopics() {
+	Logger.Println("Creating topics")
+	config := NewConfig()
+	config.Metadata.Retry.Max = 3
+	config.Metadata.Retry.Backoff = 3 * time.Second
+	var err error
+	config.Version, err = ParseKafkaVersion(os.Getenv("KAFKA_VERSION"))
+	if err != nil {
+		panic(fmt.Sprintf("Could not parse kafka version %s: %+v", os.Getenv("KAFKA_VERSION"), err))
+	}
+	client, err := NewClient(kafkaBrokers, config)
+	if err != nil {
+		panic("failed to connect to kafka")
+	}
+	defer client.Close()
+	broker, err := client.Controller()
+	if err != nil {
+		panic(fmt.Sprintf("no controller available: %+v", err))
+	}
+
+	// Delete the uncommitted-topic-test-4 topic (which is used by
+	/// https://github.com/FrancoisPoinsot/simplest-uncommitted-msg/blob/master/src/main/java/CustomProducer/Main.java
+	// in TestReadOnlyAndAllCommittedMessages), so we can re-create it empty, and run the test.k
+	deleteRes, err := broker.DeleteTopics(&DeleteTopicsRequest{
+		Topics: []string{"uncomitted-topic-test-4", "test.1", "test.4", "test.64"},
+		Timeout: 3 * time.Second,
+	})
+	if err != nil {
+		panic(fmt.Sprintf("Could not delete the uncomitted 4 topic: %+v", err))
+	}
+	for _, topicErr := range deleteRes.TopicErrorCodes {
+		if topicErr != ErrUnknownTopicOrPartition && topicErr != ErrInvalidTopic && topicErr != ErrNoError {
+			panic(fmt.Sprintf("failed to delete topic: %+v", topicErr))
+		}
+	}
+
+	// We need to wait a while for the deletes to process
+	topicsOk := false
+mdloop:
+	for i := 0; i < 20; i++{
+		time.Sleep(1 * time.Second)
+		md, err := broker.GetMetadata(&MetadataRequest{
+			Topics: []string{"uncomitted-topic-test-4", "test.1", "test.4", "test.64"},
+		})
+		if err != nil {
+			panic(fmt.Sprintf("failed to call GetMetadata: %+v", err))
+		}
+		for _, topicMd := range md.Topics {
+			if topicMd.Err != ErrUnknownTopicOrPartition && topicMd.Err != ErrInvalidTopic && topicMd.Err != ErrNoError {
+				// Need to try again
+				continue mdloop
+			}
+		}
+		topicsOk = true
+		break
+	}
+	if !topicsOk {
+		panic(fmt.Sprintf("timout waiting for topics to be OK"))
+	}
+
+	createRes, err := broker.CreateTopics(&CreateTopicsRequest{
+		TopicDetails: map[string]*TopicDetail{
+			"test.1": {
+				NumPartitions: 1,
+				ReplicationFactor: 3,
+			},
+			"test.4": {
+				NumPartitions: 4,
+				ReplicationFactor: 3,
+			},
+			"test.64": {
+				NumPartitions: 64,
+				ReplicationFactor: 3,
+			},
+			"uncommitted-topic-test-4": {
+				NumPartitions: 1,
+				ReplicationFactor: 3,
+			},
+		},
+		Timeout: 3 * time.Second,
+	})
+	if err != nil {
+		panic(fmt.Sprintf("could not create topics: %+v", err))
+	}
+	for topic, topicErr := range createRes.TopicErrors {
+		if topicErr.Err != ErrTopicAlreadyExists && topicErr.Err != ErrNoError {
+			panic(fmt.Sprintf("failed to create topic %s: %+v", topic, topicErr))
+		}
+	}
+
+	// Now fill the topic with the java blob.
+	c := exec.Command("wget", "-nc", "https://github.com/FrancoisPoinsot/simplest-uncommitted-msg/releases/download/0.1/simplest-uncommitted-msg-0.1-jar-with-dependencies.jar")
+	c.Stderr = os.Stderr
+	c.Stdout = os.Stdout
+	err = c.Run()
+	if err != nil {
+		panic(fmt.Sprintf("failed to download jre blob: %+v", err))
+	}
+	c = exec.Command("java", "-jar", "simplest-uncommitted-msg-0.1-jar-with-dependencies.jar", "-b", kafkaBrokers[0], "-c", "4")
+	c.Stderr = os.Stderr
+	c.Stdout = os.Stdout
+	err = c.Run()
+	if err != nil {
+		panic(fmt.Sprintf("failed to run java consumer: %+v", err))
+	}
+
+	Logger.Println("Created topics OK.")
 }
